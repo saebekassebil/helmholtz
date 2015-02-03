4a38924c41e6b4d9a777b0b884bbc3e7417c2536
Jakob Miland <saebekassebil@gmail.com>
2015-02-03 22:45:27 +0100

    Update tests

--- test/helmholtz.js
+++ test/helmholtz.js
@@ -2,12 +2,12 @@ var test = require('tape'),
   helmholtz = require('../');
 
 test('parsing helmholtz notation', function(t) {
-  t.deepEqual(helmholtz('a\''), [3, 3]);
-  t.deepEqual(helmholtz(',,C'), [0, 0]);
-  t.deepEqual(helmholtz('c#'), [-1, 7]);
-  t.deepEqual(helmholtz('D'), [1, 2]);
-  t.deepEqual(helmholtz('Ebb,,'), [6, -10]);
-  t.deepEqual(helmholtz('gx\'\'\''), [-2, 15]);
-  t.deepEqual(helmholtz('fb'), [8, -8]);
+  t.deepEqual(helmholtz('a\''), [0, 0]);
+  t.deepEqual(helmholtz(',,C'), [-3, -3]);
+  t.deepEqual(helmholtz('c#'), [-4, 4]);
+  t.deepEqual(helmholtz('D'), [-2, -1]);
+  t.deepEqual(helmholtz('Ebb,,'), [3, -13]);
+  t.deepEqual(helmholtz('gx\'\'\''), [-5, 12]);
+  t.deepEqual(helmholtz('fb'), [5, -11]);
   t.end();
 });
